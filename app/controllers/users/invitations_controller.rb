a1632359f8d76e7c32385874fffb2184d170d270
Wellyson Lopes <107424893+Wellyson-Lopes@users.noreply.github.com>
2026-06-02 01:07:54 -0300

    chore(lint): fix rubocop offenses in invitations and routes

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
-class Users::InvitationsController < Devise::InvitationsController
-  before_action :authenticate_inviter!, only: %i[new create]
-  before_action :ensure_admin!, only: %i[new create]
+module Users
+  class InvitationsController < Devise::InvitationsController
+    before_action :authenticate_inviter!, only: %i[new create]
+    before_action :ensure_admin!, only: %i[new create]
 
-  protected
+    protected
 
-  def ensure_admin!
-    unless current_user&.admin?
+    def ensure_admin!
+      return if current_user&.admin?
+
       redirect_to dashboard_index_path, alert: 'Você não tem permissão para convidar novos usuários.'
     end
   end

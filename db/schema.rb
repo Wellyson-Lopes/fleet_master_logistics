df4d7afdbf0d2cb5386251e0cf303835da8f2c26
Wellyson Lopes <107424893+Wellyson-Lopes@users.noreply.github.com>
2026-06-06 18:28:33 -0300

    feat(Infra): adiciona suporte a JWT e CORS para aplicativo móvel [sc-32]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_05_31_021237) do
+ActiveRecord::Schema[8.0].define(version: 2026_06_06_210429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -71,10 +71,12 @@ ActiveRecord::Schema[8.0].define(version: 2026_05_31_021237) do
     t.integer "invitations_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "jti"
     t.index ["cnpj"], name: "index_users_on_cnpj", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
+    t.index ["jti"], name: "index_users_on_jti"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 

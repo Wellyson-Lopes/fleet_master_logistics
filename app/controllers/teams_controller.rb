d0738bfccc9b0dde95335fb12d7e53e9d27f4872
Wellyson Lopes <107424893+Wellyson-Lopes@users.noreply.github.com>
2026-06-21 01:01:01 -0300

    refactor: simplifica TeamsController removendo Authorizable desnecessário
    - Remove include Authorizable (não existe TeamPolicy)
    - Usa before_action padrão em vez de prepend_before_action
    - Mantém UserPolicy::Scope para filtragem de usuários

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class TeamsController < ApplicationController
-  include Authorizable
-
-  prepend_before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @users = UserPolicy::Scope.new(current_user, User).resolve
